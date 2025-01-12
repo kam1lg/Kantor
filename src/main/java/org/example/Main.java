c3d300255b0073541fba874b93065103a26b8e87
Kamil Gręda <kamil.greda@student.up.krakow.pl>
2025-01-12 16:33:51 +0100

    pliki niezbędne do projektu

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,6 +1,5 @@
 package org.example;
 
-import org.example.exchangers.Exchanger;
 import org.example.models.Rates;
 import org.example.service.DbService;
 import org.example.service.KryptoService;
@@ -10,6 +9,7 @@ import org.knowm.xchange.dto.marketdata.Ticker;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.nio.file.Paths;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Date;
@@ -19,67 +19,67 @@ import java.util.stream.Collectors;
 import static org.example.utils.Retriever.retrieveRates;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, SQLException {
         /*List<Rates> rates = retrieveRates();
         Exchanger exchanger = new Exchanger(rates);
         ExchangerHandler exchangerHandler = new ExchangerHandler(exchanger);
         exchangerHandler.handleExchanger(args);*/
 
-                // Pobranie wszystkich walut z NBP
-                List<Rates> ratesList = getAllCurrenciesFromNBP();
+        // Pobranie wszystkich walut z NBP
+        List<Rates> ratesList = getAllCurrenciesFromNBP();
 
-                // Wyświetlenie wszystkich walut i ich kursów
-                /*for (Rates rate : ratesList) {
-                    System.out.println(rate.code() + ": " + rate.mid());
-                }*/
+        // Wyświetlenie wszystkich walut i ich kursów
+        /*for (Rates rate : ratesList) {
+            System.out.println(rate.code() + ": " + rate.mid());
+        }*/
 
-                // Zapisanie kursów walut do tabeli kursy_fiat
-                saveFiatRates(ratesList);
+        // Zapisanie kursów walut do tabeli kursy_fiat
+        saveFiatRates(ratesList);
 
-                // Pobranie kursów kryptowalut i zapisanie do tabeli kursy_krypto
-                saveCryptoRates();
-            }
+        // Pobranie kursów kryptowalut i zapisanie do tabeli kursy_krypto
+        saveCryptoRates();
+    }
 
-            // Funkcja pobierająca wszystkie dostępne waluty z API NBP
-            public static List<Rates> getAllCurrenciesFromNBP() throws IOException {
-                return Retriever.retrieveRates();
-            }
+    // Funkcja pobierająca wszystkie dostępne waluty z API NBP
+    public static List<Rates> getAllCurrenciesFromNBP() throws IOException {
+        return Retriever.retrieveRates();
+    }
 
-            // Funkcja zapisująca kursy walut do tabeli kursy_fiat
-            public static void saveFiatRates(List<Rates> ratesList) {
-                DbService dbService = new DbService();
-                SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
-                SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
-                String date = sdfDate.format(new Date());
-                String time = sdfTime.format(new Date());
+    // Funkcja zapisująca kursy walut do tabeli kursy_fiat
+    public static void saveFiatRates(List<Rates> ratesList) {
+        DbService dbService = new DbService();
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
+        String date = sdfDate.format(new Date());
+        String time = sdfTime.format(new Date());
 
-                for (Rates rate : ratesList) {
-                    try {
-                        dbService.insertFiatRate(date, time, rate.code(), rate.mid());
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
-            // Funkcja zapisująca kursy kryptowalut do tabeli kursy_krypto
-            public static void saveCryptoRates() throws IOException {
-                DbService dbService = new DbService();
-                KryptoService serwisKrypto = new KryptoService();
-                List<String> cryptoCurrencies = Arrays.asList("BTC", "ETH", "LTC", "XRP", "BCH");
-                SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
-                SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
-                String date = sdfDate.format(new Date());
-                String time = sdfTime.format(new Date());
-
-                for (String crypto : cryptoCurrencies) {
-                    try {
-                        Ticker ticker = serwisKrypto.getData(crypto, "USD");
-                        Double rate = ticker.getLast().doubleValue();
-                        dbService.insertCryptoRate(date, time, crypto, rate);
-                    } catch (Exception e) {
-                        e.printStackTrace();
-                    }
-                }
+        for (Rates rate : ratesList) {
+            try {
+                dbService.insertFiatRate(date, time, rate.code(), rate.mid());
+            } catch (Exception e) {
+                e.printStackTrace();
             }
         }
+    }
+
+    // Funkcja zapisująca kursy kryptowalut do tabeli kursy_krypto
+    public static void saveCryptoRates() throws IOException {
+        DbService dbService = new DbService();
+        KryptoService serwisKrypto = new KryptoService();
+        List<String> cryptoCurrencies = Arrays.asList("BTC", "ETH", "LTC", "XRP", "BCH");
+        SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd");
+        SimpleDateFormat sdfTime = new SimpleDateFormat("HH:mm:ss");
+        String date = sdfDate.format(new Date());
+        String time = sdfTime.format(new Date());
+
+        for (String crypto : cryptoCurrencies) {
+            try {
+                Ticker ticker = serwisKrypto.getData(crypto, "USD");
+                Double rate = ticker.getLast().doubleValue();
+                dbService.insertCryptoRate(date, time, crypto, rate);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- src/main/java/org/example/service/DbService.java
+++ src/main/java/org/example/service/DbService.java
@@ -14,11 +14,9 @@ public class DbService {
         return DriverManager.getConnection(url, user, password);
     }
 
-
     // Metoda do inserty do tabeli "kursy_fiat" za pomocą pstmt
     public void insertFiatRate(String date, String time, String code, Double rate) throws SQLException {
         String sql = "INSERT INTO kursy_fiat (data, czas, code, rate) VALUES (?, ?, ?, ?)";
-
         try (Connection conn = connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, date);
             pstmt.setString(2, time);
@@ -31,7 +29,6 @@ public class DbService {
     // Metoda do inserty do tabeli "kursy_krypto" za pomocą pstmt
     public void insertCryptoRate(String date, String time, String code, Double rate) throws SQLException {
         String sql = "INSERT INTO kursy_krypto (data, czas, code, rate) VALUES (?, ?, ?, ?)";
-
         try (Connection conn = connect(); PreparedStatement pstmt = conn.prepareStatement(sql)) {
             pstmt.setString(1, date);
             pstmt.setString(2, time);
